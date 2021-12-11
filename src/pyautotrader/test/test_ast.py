bf9f9c2f24a94422299a9df174b6d45c3a380239
Guilherme Bencke <gbencke@benckesoftware.com.br>
2021-12-11 20:43:29 -0300

    debug(pyAutoTrader):Ignoring generated python files from booster

--- src/pyautotrader/test/test_ast.py
+++ src/pyautotrader/test/test_ast.py
@@ -7,3 +7,8 @@ if __name__ == '__main__':
     python_script_name = 'process_long'
     export_model_python(ast, python_script_name, os.path.join(
         os.path.dirname(__file__), python_script_name + '.py'))
+
+    ast = create_ast_from_xgboost_dump(sys.argv[2])
+    python_script_name = 'process_short'
+    export_model_python(ast, python_script_name, os.path.join(
+        os.path.dirname(__file__), python_script_name + '.py'))
