7f27d1069399c146c1c953cc32ccf09762c98f36
Guilherme Bencke <gbencke@benckesoftware.com.br>
2022-03-08 13:45:08 +0000

    test(pyAutoTrader):Fix the run scennarios script...

--- run_scenarios.sh
+++ run_scenarios.sh
@@ -8,13 +8,26 @@ export PYTHONPATH=$PWD/src
 
 cd src/pyautotrader
 
-python __main__.py run_scenarios --minimum-interactions 3
+export CURRENT_TARGET=0.8
+export CURRENT_STOP=0.4
+
+export CURRENT_5MIN_FILE_CSV='WDO$M5.csv'
+export CURRENT_TIMEFRAME=5Min
+export MAX_TRADE_DURATION=24
+python __main__.py run_scenarios --minimum-interactions 2
+
+export CURRENT_5MIN_FILE_CSV='WDO$M10.csv'
+export CURRENT_TIMEFRAME=10Min
+export MAX_TRADE_DURATION=12
+python __main__.py run_scenarios --minimum-interactions 2
+
+export CURRENT_5MIN_FILE_CSV='WDO$M15.csv'
+export CURRENT_TIMEFRAME=15Min
+export MAX_TRADE_DURATION=8
+python __main__.py run_scenarios --minimum-interactions 2
 
 cd ../strategies/B3/WDOL/00.data/
 
-export strategy7z="$(date '+%Y%m%d%H%M%S').strategies.7z"
-
-7z a -mx9 $strategy7z strategies
 
 
 
