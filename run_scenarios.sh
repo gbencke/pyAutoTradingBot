6a2e33fe542bd2cf04c24b54ea81e3ab2c653c8f
Guilherme Bencke <gbencke@benckesoftware.com.br>
2022-03-20 19:35:08 -0300

    test(pyAutoTrader):Minimal generation of scenarios...

--- run_scenarios.sh
+++ run_scenarios.sh
@@ -6,6 +6,8 @@ source ./clean_notebooks.sh
 
 export PYTHONPATH=$PWD/src
 
+set PYAUTOTRADER_ROOT=$PWD
+
 cd src/pyautotrader
 
 export CURRENT_TARGET=1.2
@@ -13,71 +15,23 @@ export CURRENT_STOP=0.7
 export CURRENT_5MIN_FILE_CSV='WDO$M10.csv'
 export CURRENT_TIMEFRAME=10Min
 export MAX_TRADE_DURATION=24
-python __main__.py run_scenarios --minimum-interactions 20
-
-export CURRENT_TARGET=1.2
-export CURRENT_STOP=0.6
-export CURRENT_5MIN_FILE_CSV='WDO$M10.csv'
-export CURRENT_TIMEFRAME=10Min
-export MAX_TRADE_DURATION=24
-python __main__.py run_scenarios --minimum-interactions 20
-
-export CURRENT_TARGET=1.2
-export CURRENT_STOP=0.5
-export CURRENT_5MIN_FILE_CSV='WDO$M10.csv'
-export CURRENT_TIMEFRAME=10Min
-export MAX_TRADE_DURATION=24
-python __main__.py run_scenarios --minimum-interactions 20
-
-export CURRENT_TARGET=1.0
-export CURRENT_STOP=0.7
-export CURRENT_5MIN_FILE_CSV='WDO$M10.csv'
-export CURRENT_TIMEFRAME=10Min
-export MAX_TRADE_DURATION=24
-python __main__.py run_scenarios --minimum-interactions 20
-
-export CURRENT_TARGET=1.0
-export CURRENT_STOP=0.6
-export CURRENT_5MIN_FILE_CSV='WDO$M10.csv'
-export CURRENT_TIMEFRAME=10Min
-export MAX_TRADE_DURATION=24
-python __main__.py run_scenarios --minimum-interactions 20
-
-export CURRENT_TARGET=1.0
-export CURRENT_STOP=0.5
-export CURRENT_5MIN_FILE_CSV='WDO$M10.csv'
-export CURRENT_TIMEFRAME=10Min
-export MAX_TRADE_DURATION=24
-python __main__.py run_scenarios --minimum-interactions 20
-
-export CURRENT_TARGET=0.8
-export CURRENT_STOP=0.7
-export CURRENT_5MIN_FILE_CSV='WDO$M10.csv'
-export CURRENT_TIMEFRAME=10Min
-export MAX_TRADE_DURATION=24
-python __main__.py run_scenarios --minimum-interactions 20
-
-export CURRENT_TARGET=0.8
-export CURRENT_STOP=0.6
-export CURRENT_5MIN_FILE_CSV='WDO$M10.csv'
-export CURRENT_TIMEFRAME=10Min
-export MAX_TRADE_DURATION=24
-python __main__.py run_scenarios --minimum-interactions 20
-
-export CURRENT_TARGET=0.8
-export CURRENT_STOP=0.5
-export CURRENT_5MIN_FILE_CSV='WDO$M10.csv'
-export CURRENT_TIMEFRAME=10Min
-export MAX_TRADE_DURATION=24
-python __main__.py run_scenarios --minimum-interactions 20
-
+python __main__.py run_scenarios --minimum-interactions 2
 
 python __main__.py summarize_scenarios
 
+cd ../../utils
+
+python ./generate_pnl_charts.py
+
+cd $PYAUTOTRADER_ROOT
+
 cd ../strategies/B3/WDOL/00.data/
 
+export strategy7z="$(date '+%Y%m%d%H%M%S').strategies.7z"
 
+7z a -mx9 $strategy7z strategies
 
+cd $PYAUTOTRADER_ROOT
 
 
 
