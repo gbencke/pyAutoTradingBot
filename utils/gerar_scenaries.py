5893489d23501962e70ac9b247ea34aa8c998324
Guilherme Bencke <gbencke@benckesoftware.com.br>
2022-04-05 17:25:11 -0300

    test(pyAutoTrader):Last Test, now with 5Min charts...

--- utils/gerar_scenaries.py
+++ utils/gerar_scenaries.py
@@ -1,10 +1,8 @@
 
 
-TIMEFRAMES = [
-        {'timeframe': '10Min', 'file': 'WDO$M10.csv', 'tradeduration': 24},
-        {'timeframe': '5Min', 'file': 'WDO$M5.csv', 'tradeduration': 48}]
+TIMEFRAMES = [ {'timeframe': '5Min', 'file': 'WDO$M5.csv', 'tradeduration': 24}]
 
-CURRENT_TARGET = [1.0, 0.9, 0.8, 0.7,0.6,0.5, 0.4]
+CURRENT_TARGET = [0.9, 0.8, 0.7,0.6,0.5, 0.4]
 CURRENT_STOP = [0.6,0.5,0.4, 0.3]
 
 for curTIMEFRAME in TIMEFRAMES:
