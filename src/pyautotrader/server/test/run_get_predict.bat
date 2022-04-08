37b8cb008852bc5e8874140ee8b9d5dfbb577769
Guilherme Bencke <gbencke@benckesoftware.com.br>
2022-04-08 11:27:58 -0300

    test(pyAutoTrader):Correction of test scripts...

--- src/pyautotrader/server/test/run_get_predict.bat
+++ src/pyautotrader/server/test/run_get_predict.bat
@@ -1,2 +1,2 @@
 
-curl -vvv -H "Content-Type: application/json" http://127.0.0.1:5000/predict/B3/WDOL/10Min/20220218/0930/
+curl -vvv -H "Content-Type: application/json" http://127.0.0.1:5000/predict/B3/WDO/10Min/20220218/0930/
