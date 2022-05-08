186cc58a337bf5905f567827edca305c879372bc
Guilherme Bencke <gbencke@benckesoftware.com.br>
2022-05-07 22:37:06 -0300

    test(pyAutoTrader):Setting parameters of the model...

--- run_notebooks.bat
+++ run_notebooks.bat
@@ -4,6 +4,16 @@ call .\env\Scripts\activate.bat
 
 call .\clean_notebooks.bat
 
+set PYTHONPATH=src
+set USAR_SMART_STOP=0
+set CURRENT_TARGET=1.0
+set CURRENT_STOP=0.4
+set CURRENT_5MIN_FILE_CSV=WDO$M5.csv
+set CURRENT_TIMEFRAME=5Min
+set MAX_TRADE_DURATION=48
+set PYAUTOTRADER_URL=http://localhost:5000/
+set DATA_OUTPUT_DIR=\git\216\src\strategies\B3\WDOL\00.data\output\
+
 jupyter nbconvert --inplace --to notebook --ExecutePreprocessor.timeout=-1 --execute .\src\strategies\B3.WDOL\02.candle_strategy\02.candle_strategy_0100_create_dataframe.ipynb
  
 jupyter nbconvert --inplace  --to notebook --ExecutePreprocessor.timeout=-1 --execute .\src\strategies\B3.WDOL\02.candle_strategy\02.candle_strategy_0200_create_xgbooster.ipynb
