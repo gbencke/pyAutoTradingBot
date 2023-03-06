edfc81a51bba808eb50b0b7f7559f1d405a3cb2c
Guilherme Bencke <gbencke@benckesoftware.com.br>
2023-03-06 10:12:12 -0300

    build(pyAutoTrader):Refactor Src tree

--- import_csv_from_profit_chart.bat
+++
@@ -1,9 +0,0 @@
-cd %PYAUTOTRADER_ROOT%
-
-set SOURCE_DIR=%cd%
-
-set PYTHONPATH=\git\216\src
-
-.\env\Scripts\activate.bat & cd utils & python import_data_from_profit.py
-
-
--- run_server_profiler.bat
+++
@@ -1,8 +0,0 @@
-call .\env\Scripts\activate.bat
-
-set PYTHONPATH=%cd%\src
-
-cd src\pyautotrader
-
-python -m cProfile -o saida.out __main__.py start_server --sqlalchemy-connection-string sqlite:///data//pyautotrader.db --xgboost-model C:\git\216\src\strategies\B3\WDOL\00.data\output
-
