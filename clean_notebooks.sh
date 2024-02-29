4950d8f2ee5e39c841bfb6f2995e4eb8c8bd9dfc
Guilherme Bencke <gbencke@benckesoftware.com.br>
2024-02-29 17:26:16 -0300

    Added Jupyter to project and also fixed clean notebooks

--- clean_notebooks.sh
+++ clean_notebooks.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-source ./env/bin/activate
-
-jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace ./models/strategies/B3/WDOL/02.candle_strategy/02.candle_strategy_0100_create_dataframe.ipynb
+poetry run jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace ./models/strategies/B3/WDOL/02.candle_strategy/02.candle_strategy_0100_create_dataframe.ipynb
  
-jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace ./models/strategies/B3/WDOL/02.candle_strategy/02.candle_strategy_0200_create_xgbooster.ipynb
+poetry run jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace ./models/strategies/B3/WDOL/02.candle_strategy/02.candle_strategy_0200_create_xgbooster.ipynb
 
-jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace ./models/strategies/B3/WDOL/02.candle_strategy/02.candle_strategy_0300_find_decision_boundary.ipynb
+poetry run jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace ./models/strategies/B3/WDOL/02.candle_strategy/02.candle_strategy_0300_find_decision_boundary.ipynb
 
-jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace ./models/strategies/B3/WDOL/02.candle_strategy/02.candle_strategy_0400_forward_testing.ipynb
+poetry run jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace ./models/strategies/B3/WDOL/02.candle_strategy/02.candle_strategy_0400_forward_testing.ipynb
 
